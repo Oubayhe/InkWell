a3389ede65becac4ebe81335ac6c00fcc8b17cd5
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-02-15 17:32:30 +0100

    Starting the travel together app, after installing the frontend client side, meaning both react using vite command and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-xl p-14 bg-red-300 text-green-600'>
+      Welcome to Travel Together
+    </div>
+  )
+}
+
+export default App

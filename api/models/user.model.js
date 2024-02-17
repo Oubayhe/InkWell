0f21e4d621fdc1a3d8e08a5c0f62f1b7801b3d44
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-02-17 14:04:24 +0100

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {timestamps: true})
+// timestamps is for the creating the update_date and create_date automaticaly
+
+module.exports = mongoose.model('User', userSchema)

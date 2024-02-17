35f912b0e6d5a157593b3d1051a629ba42d8d945
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-02-17 13:54:01 +0100

    Setting the Database connection with MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
+// for .env variables
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const express = require('express')
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('Server is running...')
+        })
+    })
 
 const app = express()
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000')
-})

edb1e7645b6af752f4cb75f1d8d73f46ba4e91ec
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-03-24 06:39:00 +0000

    Create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,5 +5,6 @@ const postController = require('../controllers/post.controller')
 const router = express.Router()
 
 router.post('/create', verifyToken, postController.createPost)
+router.get('/getposts',  postController.getPost)
 
 module.exports = router

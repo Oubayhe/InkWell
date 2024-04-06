67c86393eb1f5008cd95f13f7419e5f976b7210e
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-04-06 15:48:19 +0000

    Adding the delete functionality and API

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,8 +5,8 @@ const postController = require('../controllers/post.controller')
 const router = express.Router()
 
 router.post('/create', verifyToken, postController.createPost)
-
 // We're not using verifyToken because we want everybody to be able to see the posts
 router.get('/getposts',  postController.getPost)
+router.delete('/deletepost/:postId/:userId', verifyToken, postController.deletePost)
 
 module.exports = router

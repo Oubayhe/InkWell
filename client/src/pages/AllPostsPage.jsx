78ebf19a2d0accbd719235ded5116f9d6e9e655b
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-04-14 13:01:28 +0100

    Add the other functionnalities of filtering. Adding the sorting, category, username.

--- client/src/pages/AllPostsPage.jsx
+++ client/src/pages/AllPostsPage.jsx
@@ -5,20 +5,23 @@ import { useNavigate } from 'react-router-dom'
 import { Button, Select, Spinner } from 'flowbite-react'
 
 export default function AllPostsPage({searchTerm}) {
+    const [noPostFound, setNoPostFound] = useState(false)
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true)
     const [limit, setLimit] = useState(6)
     const [startIndex, setStartIndex] = useState(0)
     const [search, setSearch] = useState('')
-    const [sort, setSort] = useState(1)
+    const [order, setOrder] = useState('des')
     const [users, setUsers] = useState([])
+    const [category, setCategory] = useState('')
+    const [userId, setUserId] = useState('')
     const navigate = useNavigate()
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 setLoading(true)
-                const res = await fetch(`/api/post/getposts?searchTerm=${searchTerm}`)
+                const res = await fetch(`/api/post/getposts?searchTerm=${searchTerm}&&limit=${limit}`)
                 const data = await res.json()
                 if (res.ok) {
                     setPosts(data.posts)
@@ -95,10 +98,13 @@ export default function AllPostsPage({searchTerm}) {
         e.preventDefault()
         try {
             setLoading(true)
-            const res = await fetch(`/api/post/getposts?searchTerm=${search}`)
+            const res = await fetch(`/api/post/getposts?searchTerm=${search}&&category=${category}&&userId=${userId}&&order=${order}&&limit=${limit}`)
+            // const &&startIndex=${startIndex}
+            // const res = await fetch(`/api/post/getposts?searchTerm=${search}&&category=${category}`)
             const data = await res.json()
             if (res.ok) {
                 setPosts(data.posts)
+                setNoPostFound(data.posts.length === 0)
                 setLoading(false)
             }
             if (!res.ok) {
@@ -112,16 +118,18 @@ export default function AllPostsPage({searchTerm}) {
     }
 
     
-    if (posts.length == 0 || users.length == 0 || loading) return (
+    if ((posts.length == 0 || users.length == 0 || loading) && !noPostFound) return (
         <div className="flex justify-center items-center h-screen">
+            {console.log(posts, noPostFound, category, search)}
             <Spinner size="xl" />
         </div>
     )
   return (
-    
-    <div className="flex justify-center relative">
+
+    <div className="flex relative min-h-screen">
+        {console.log(posts, noPostFound, category, search)}
         {/* The Filtering Sidebar */}
-        <div className="w-3/5 border-r-2 dark:bg-slate-600 bg-gray-50 border-gray-200 dark:border-slate-700 ">
+        <div className="w-1/5 border-r-2 dark:bg-slate-600 bg-gray-50 border-gray-200 dark:border-slate-700 ">
             <form onSubmit={handleSubmit} className='mx-4 my-8 flex flex-col gap-2'>
             <div className="relative">
                 <input onChange={(e) => {setSearch(e.target.value)}} type="text" id="small_outlined" className="block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 
@@ -133,16 +141,21 @@ export default function AllPostsPage({searchTerm}) {
                 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 
                 peer-focus:-translate-y-3 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Search term</label>
             </div>
-            <Select>
-                <option value="All">All</option>
+            <Select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">All Categories</option>
                 {categories.map((categoryItem) => {
                     return (
-                        <option key={categories.indexOf(categoryItem)} value={categoryItem}>{categoryItem} </option>
+                        <option 
+                            key={categories.indexOf(categoryItem)} 
+                            value={categoryItem}
+                        > 
+                            {categoryItem} 
+                        </option>
                     )
                 })}
             </Select>
-            <Select>
-                <option value="All">All</option>
+            <Select value={userId} onChange={(e) => setUserId(e.target.value)}>
+                <option value="">All users</option>
                 {users.map((user) => {
                     return (
                         <option 
@@ -155,17 +168,20 @@ export default function AllPostsPage({searchTerm}) {
                     )
                 })}
             </Select>
-            <Select>
-                <option value={-1}>Oldest</option>
-                <option value={1}>Newest</option>
+            <Select value={order} onChange={(e) => setOrder(e.target.value)}>
+                <option value='des'>Newest</option>
+                <option value='asc'>Oldest</option>
             </Select>
             <Button type='submit' className='mt-6' gradientDuoTone="purpleToBlue">Filter</Button>
             </form>
         </div>
-        <div className="">
+        {noPostFound ? (
+                <h1 className='text-5xl font-bold m-4'>No post was found</h1>            
+        ) : (
+        <div className="w-4/5 relative">
         {posts.map((post) => {
         return (
-            <div key={post._id} className="py-6 border-b border-gray-400 w-10/12 h-[calc(500px)] lg:w-4/5 lg:h-72 
+            <div key={post._id} className="py-6 border-b border-gray-400 h-[calc(500px)] w-11/12 lg:h-72 
             grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 mx-4 ">
                 <div className="h-full">
                     <SlideImages images={post.images} />
@@ -176,11 +192,14 @@ export default function AllPostsPage({searchTerm}) {
                     <div className="bottom-0 left-0 absolute font-bold text-gray-500 ">
                         {moment(post.createdAt).format('D MMMM YYYY')}
                     </div>
+                    <div className="bottom-0 right-0 absolute italic text-gray-500 ">
+                        {(post.content.length/1000).toFixed(0)} mins read
+                    </div>
                 </div>
             </div>
         )
     })}
-    </div>
+    </div>)}
     </div>
   )
 }

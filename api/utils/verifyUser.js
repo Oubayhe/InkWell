917b9d0b980266eb7579c838a02e11caf7ebbd4b
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-04-17 07:04:29 +0100

    Returning a better error message for the unauthorized users

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -7,11 +7,11 @@ const errorHandler = require('./error')
 const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token
     if (!token) {
-        return next(errorHandler(401, "Unauthorized"))
+        return next(errorHandler(401, "You have been logged out by the server. You need to sign in again."))
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if(err) {
-            return next(errorHandler(401, 'Unauthorized'))
+            return next(errorHandler(401, 'You have been logged out by the server. You need to sign in again.'))
         }
         req.user = user
         next()
